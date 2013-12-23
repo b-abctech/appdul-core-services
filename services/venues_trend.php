145f31aabb23c526c8220d5dca1d7c740aaffc48
b <b@abctech-thailand.com>
2013-12-23 21:37:40 +0700

    Add venues_trend service page

---
+++ services/venues_trend.php
@@ -0,0 +1,11 @@
+<?php
+    require_once('../inc/class.FoursquareConnector.inc');
+    
+    $foursquareConnector = new FoursquareConnector();
+    $json_return = $foursquareConnector->getVenuesTrending( 
+            '13.735889,100.561302',
+            '20131223'
+        );
+    header('Content-type: application/json');
+    echo $json_return;
+?>
