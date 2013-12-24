d9ee4b59d333c7f457afed859362c032a48fb65b
b <b@abctech-thailand.com>
2013-12-24 22:16:21 +0700

    Force utf8 encoding

--- services/venues_trend.php
+++ services/venues_trend.php
@@ -6,6 +6,6 @@
             '13.735889,100.561302',
             '20131223'
         );
-    header('Content-type: application/json');
+    header('Content-type: application/json; charset=utf-8');
     echo $json_return;
 ?>
